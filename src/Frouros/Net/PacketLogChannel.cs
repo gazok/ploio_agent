97aeaee8f64cef1eca471ca802a5cb18133a8e36
Sharp0802 <sharp0802.business@gmail.com>
2023-10-15 15:53:20 +0900

    Feat: Implement read/flush
    - src/Frouros/Net/PacketLogChannel.cs: Implement read/flush
    - src/Frouros/Utils/StreamUtils.cs: Implement stream copy

--- src/Frouros/Net/PacketLogChannel.cs
+++ src/Frouros/Net/PacketLogChannel.cs
@@ -1,16 +1,16 @@
-//    Copyright 2023 Yeong-won Seo
+//     Copyright 2023 Yeong-won Seo
 // 
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
 // 
-//        http://www.apache.org/licenses/LICENSE-2.0
+//         http://www.apache.org/licenses/LICENSE-2.0
 // 
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
 
 using Frouros.Primitives;
 using Frouros.Utils;
@@ -18,7 +18,7 @@ using Microsoft.Win32.SafeHandles;
 
 namespace Frouros.Net;
 
-public class PacketLogBuffer : IDisposable
+public class PacketLogChannel : IDisposable
 {
     // Buffer size must be multiple of BufferSize
     private const int FileSize = BufferSize * 4096;
@@ -35,7 +35,7 @@ public class PacketLogBuffer : IDisposable
     private long _fileOffset;
     private int  _offset;
     
-    public PacketLogBuffer()
+    public PacketLogChannel()
     {
         var filepath = Path.GetTempFileName();
 
@@ -100,7 +100,6 @@ public class PacketLogBuffer : IDisposable
         }
 
         log.AsNative().TryWriteTo(_buffer.GetBuffer(), ofs);
-        return;
     }
 
     public void Read(Stream stream)
---
+++ src/Frouros/Utils/StreamUtils.cs
@@ -0,0 +1,34 @@
+﻿//     Copyright 2023 Yeong-won Seo
+// 
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+// 
+//         http://www.apache.org/licenses/LICENSE-2.0
+// 
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+
+using System.Buffers;
+
+namespace Frouros.Utils;
+
+public static class StreamUtils
+{
+    public static void CopyTo(this Stream src, Stream dst, long bytes, int bufferSize)
+    {
+        var buffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+
+        while (bytes > 0)
+        {
+            var read = src.Read(buffer);
+            dst.Write(buffer, 0, read);
+            bytes -= read;
+        }
+        
+        ArrayPool<byte>.Shared.Return(buffer);
+    }
+}
