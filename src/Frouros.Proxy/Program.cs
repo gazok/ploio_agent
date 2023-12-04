00e4049e407cc06a7f219edd21aa32552ddbaea8
Sharp0802 <sharp0802.business@gmail.com>
2023-12-05 00:02:20 +0900

    #42 Feat: Complete proxy
    - src/Frouros.Proxy/Program.cs: Complete proxy

---
+++ src/Frouros.Proxy/Program.cs
@@ -0,0 +1,41 @@
+using System.Runtime;
+using Frouros.Proxy.Bridges;
+using Frouros.Proxy.Bridges.Abstract;
+using Frouros.Proxy.Repositories;
+using Frouros.Proxy.Repositories.Abstract;
+using Frouros.Proxy.Services;
+using Frouros.Proxy.Services.Abstract;
+using Frouros.Proxy.Workers;
+using Frouros.Proxy.Workers.Routing;
+
+GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+        
+       .AddSingleton<IMembrane, Membrane>()
+        
+       .AddSingleton<IApplicationInformation, ApplicationInformation>()
+       .AddSingleton<IARPTable, ARPTable>()
+       .AddSingleton<ICAMTable, CAMTable>()
+       .AddSingleton<IModuleRepository, ModuleRepository>()
+       .AddSingleton<IPodAuthRepository, PodAuthRepository>()
+        
+       .AddSingleton<IGrpcServiceProvider, GrpcServiceProvider>()
+        
+       .AddHostedService<LogRouting>()
+       .AddHostedService<ModuleRouting>()
+       .AddHostedService<PacketRouting>()
+       .AddHostedService<PodRouting>()
+        
+       .AddHostedService<CRIWorker>()
+       .AddHostedService<PVIWorker>()
+        
+       .AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<ARPService>();
+
+app.Run();
